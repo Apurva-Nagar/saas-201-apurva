d0b6e8a9f0e40c7780a2e9777cc06fba525d23e7
Apurva <apurvanagar@gmail.com>
2021-01-16 18:28:26 +0530

    initial commit

---
+++ level-2-DNS-resolver/lookup.rb
@@ -0,0 +1,53 @@
+def get_command_line_argument
+  # ARGV is an array that Ruby defines for us,
+  # which contains all the arguments we passed to it
+  # when invoking the script from the command line.
+  # https://docs.ruby-lang.org/en/2.4.0/ARGF.html
+  if ARGV.empty?
+    puts "Usage: ruby lookup.rb <domain>"
+    exit
+  end
+  ARGV.first
+end
+
+# `domain` contains the domain name we have to look up.
+domain = get_command_line_argument
+
+# File.readlines reads a file and returns an
+# array of string, where each element is a line
+# https://www.rubydoc.info/stdlib/core/IO:readlines
+dns_raw = File.readlines("zone")
+
+def parse_dns(dns_raw)
+  dns_cleaned = dns_raw.reject { |s| s.strip.empty? || s.start_with?("#") }
+  dns_hash = {}
+  # SOURCE => [RECORD TYPE, DESTINATION]
+  dns_cleaned.map { |s| dns_hash[s.split(",")[1].strip] = [s.split(",")[0].strip, s.split(",")[2].strip] }
+  return dns_hash
+end
+
+def resolve(dns_records, lookup_chain, domain)
+  begin
+    destination = dns_records[domain][1]
+    record_type = dns_records[domain][0]
+  rescue
+    lookup_chain.push("ERROR: Unkown record type or alias non-existant.")
+    return lookup_chain
+  end
+
+  lookup_chain.push(destination)
+
+  if record_type == "CNAME"
+    resolve(dns_records, lookup_chain, destination)
+  elsif record_type == "A"
+    return lookup_chain
+  end
+end
+
+# To complete the assignment, implement `parse_dns` and `resolve`.
+# Remember to implement them above this line since in Ruby
+# you can invoke a function only after it is defined.
+dns_records = parse_dns(dns_raw)
+lookup_chain = [domain]
+lookup_chain = resolve(dns_records, lookup_chain, domain)
+puts lookup_chain.join(" => ")
