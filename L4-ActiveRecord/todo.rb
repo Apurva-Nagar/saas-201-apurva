2a8b08d3ddd37dfaeacddd4b49eddcfd4f8efe13
Apurva <apurvanagar@gmail.com>
2021-01-17 12:39:06 +0530

    initial commit

---
+++ L4-ActiveRecord/todo.rb
@@ -0,0 +1,17 @@
+require "active_record"
+
+class Todo < ActiveRecord::Base
+  def due_today?
+    due_date == Date.today
+  end
+
+  def to_displayable_string
+    display_status = completed ? "[X]" : "[ ]"
+    display_date = due_today? ? nil : due_date
+    "#{id}. #{display_status} #{todo_text} #{display_date}"
+  end
+
+  def self.to_displayable_list
+    all.map { |todo| todo.to_displayable_string }
+  end
+end
