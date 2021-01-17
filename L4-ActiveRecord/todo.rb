22f7b37881e60e8a11876ace38466cab1a3dde0a
Apurva <apurvanagar@gmail.com>
2021-01-17 12:40:53 +0530

    list-todos.rb now works

--- L4-ActiveRecord/todo.rb
+++ L4-ActiveRecord/todo.rb
@@ -5,6 +5,22 @@ class Todo < ActiveRecord::Base
     due_date == Date.today
   end
 
+  def self.show_list
+    puts "My Todo-list\n\n"
+
+    puts "Overdue\n"
+    puts all.where("due_date < ?", Date.today).to_displayable_list
+    puts "\n\n"
+
+    puts "Due Today\n"
+    puts all.where("due_date = ?", Date.today).to_displayable_list
+    puts "\n\n"
+
+    puts "Due Later\n"
+    puts all.where("due_date > ?", Date.today).to_displayable_list
+    puts "\n\n"
+  end
+
   def to_displayable_string
     display_status = completed ? "[X]" : "[ ]"
     display_date = due_today? ? nil : due_date
